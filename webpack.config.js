95bf0ebab958eb6bf4822c0f7dda254470a9bf38
usbbush <jeanbienvenusb@gmail.com>
2022-07-07 09:45:50 +0200

    Update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
@@ -27,5 +28,4 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
-  mode: 'development',
 };
